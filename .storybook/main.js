2f6fc76e1e68dbead180a3d640942a3c277c5fb8
Jan Savli <jan.savli@gmail.com>
2020-11-06 18:41:03 +0100

    override HtmlWebpackPlugin to inject stencil into storybook prod

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +9,34 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, { configType }) => {
+    // watch:storybook configType === 'DEVELOPMENT'
+    // build:storybook configType === 'PRODUCTION'
+    if (configType === 'PRODUCTION') {
+      config = doProd(config);
+    }
+
+    return config;
+  },
 }
+
+const doProd = (config) => {
+  const { name, distDirs } = require('../package.json');
+  const OUTPUT_DIR = path.join(__dirname, '../' + distDirs.stencil);
+  const mainJs = path.join(OUTPUT_DIR, `cjs/${name}.cjs.js`);
+
+  // override HtmlWebpackPlugin to inject stencil into storybook prod
+  // template default: @storybook/core/dist/server/templates/index.ejs
+  config.plugins[1].options = {
+    ...config.plugins[1].options,
+    template: path.resolve(__dirname, 'index.override.ejs'),
+  };
+  // no stencil build exists
+  if (!fs.existasSync(mainJs)) {
+    console.error(chalk.black.bgRed(`---------------------------------`));
+    console.error(chalk.black.bgRed(`Run 'npm run build:stencil' first`));
+    console.error(chalk.black.bgRed(`---------------------------------`));
+  }
+  return config;
+};
