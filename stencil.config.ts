0e04b29f342dff17e27f3bd0cf7dc9e40d8d3bdd
Jan Savli <jan.savli@gmail.com>
2020-11-06 16:44:35 +0100

    Build Stencil into dist-stencil

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,17 +1,22 @@
 import { Config } from '@stencil/core';
 
+const { name, distDirs } = require('./package.json');
+
 export const config: Config = {
-  namespace: 'zen-ui',
+  namespace: name,
   outputTargets: [
     {
       type: 'dist',
+      dir: distDirs.stencil,
       esmLoaderPath: '../loader',
     },
     {
       type: 'dist-custom-elements-bundle',
+      dir: distDirs.stencil,
     },
     {
       type: 'docs-readme',
+      dir: distDirs.stencil,
     },
     {
       type: 'www',
