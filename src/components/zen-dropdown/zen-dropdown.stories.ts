defe9583df080506e66cb6543e33ed10f06cd6fb
Jan Savli <jan.savli@gmail.com>
2020-11-07 20:10:00 +0100

    Pass options prop from storybook to component

--- src/components/zen-dropdown/zen-dropdown.stories.ts
+++ src/components/zen-dropdown/zen-dropdown.stories.ts
@@ -8,8 +8,15 @@ export default {
 const Template = () => {
   return html`
   <zen-dropdown
-    first="Jan" middle="B" last="Savli">sdjfhdf
+    options="${[{ label: '1' }, { label: '2' }]}"
   </zen-dropdown>`;
 };
 
 export const Default = Template.bind({});
+
+
+export const TextWithAction = () => {
+  const dropdown = document.createElement('zen-dropdown');
+  dropdown.options = [{ label: 'item 1' }, { label: 'item 2' }];
+  return dropdown;
+};
