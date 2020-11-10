5657126fbdc2d3ace50ae6958ba52e1bc6ece1e8
Jan Savli <jan.savli@gmail.com>
2020-11-10 15:53:08 +0100

    Add zen-dropdown story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
   "addons": [
     "@storybook/addon-essentials"
   ],
---
+++ src/components/zen-dropdown/zen-dropdown.stories.ts
@@ -0,0 +1,40 @@
+import { html } from 'lit-html';
+import { eventHandles, action } from '../../../.storybook/helpers/custom-action';
+
+const customEvents = ['input2'];
+const events = [...eventHandles(customEvents)];
+
+export default {
+  title: 'Components/Zen Dropdown',
+  component: 'zen-dropdown',
+  parameters: {
+    actions: {
+      handles: events,
+    },
+  },
+};
+
+// https://storybook.js.org/docs/react/essentials/controls#annotation
+const argTypes = {
+  selectedColor: {
+    control: 'color',
+  },
+};
+
+const Template = (
+  {
+    selectedColor,
+  }) => {
+    const opts = [{ label: 'item 12' }, { label: 'item 2' }];
+    return html`
+    <zen-dropdown
+      style="max-width: 300px"
+      .options=${opts}
+      selected-color=${selectedColor}
+    </zen-dropdown>
+    ${action('zen-dropdown', customEvents)}
+    `;
+};
+
+export const Default = Template.bind({});
+Default.argTypes = {...argTypes};
