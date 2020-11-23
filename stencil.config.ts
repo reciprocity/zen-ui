56f94e998606be0907fa2c5adbecb204e592511e
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2020-11-23 08:35:48 -0300

    Added new output target and removed unnecesary webpack file

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,11 +2,14 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'stencil-storybook-starter',
+  namespace: 'zen-ui',
   outputTargets: [
     {
       type: 'dist',
       esmLoaderPath: '../loader'
+    },
+    {
+      type: 'dist-custom-elements-bundle',
     },
     {
       type: 'docs-readme',
