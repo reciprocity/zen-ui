47e75f45b9a0bc868bed8717e4fe9e2207bce15f
Jan Savli <jan.savli@gmail.com>
2020-11-26 16:32:29 +0100

    test: Add additional test

--- src/components/zen-steps/zen-steps.spec.tsx
+++ src/components/zen-steps/zen-steps.spec.tsx
@@ -13,4 +13,45 @@ describe('zen-button', () => {
       </div>
       <ul class="steps"></ul>`);
   });
+
+  it('Renders steps', async () => {
+    const page = await newSpecPage({
+      components: [ZenSteps],
+      html: `<div></div>`,
+    });
+    let component = page.doc.createElement("zen-steps");
+
+    (component as any).steps = [{ label: 'Step one' }];
+    page.root.appendChild(component);
+    await page.waitForChanges();
+
+    expect(page.root.shadowRoot.querySelector('ul.steps')).toEqualHtml(`
+      <ul class="steps">
+        <li class="active step">
+          <div class="roundle">
+            <div>1</div>
+          </div>
+          <div class="label">Step one</div>
+        </li>
+      </ul>`);
+  });
+
+  it('Sets active step', async () => {
+    const page = await newSpecPage({
+      components: [ZenSteps],
+      html: `<div></div>`,
+    });
+    let component = page.doc.createElement("zen-steps");
+
+    (component as any).steps = [{ label: 'Step one' }, { label: 'Step two' }];
+    page.root.appendChild(component);
+    await page.waitForChanges();
+
+    expect(page.root.shadowRoot.querySelectorAll('.step').length).toEqual(2);
+    expect(page.root.shadowRoot.querySelector('.step.active .label').innerHTML).toEqualHtml('Step one');
+
+    (component as any).activeIndex = 1;
+    await page.waitForChanges();
+    expect(page.root.shadowRoot.querySelector('.step.active .label').innerHTML).toEqualHtml('Step two');
+  });
 });
