0124d955b116e7284a763322368cc1dee9632e90
Jan Savli <jan.savli@gmail.com>
2020-11-30 16:35:24 +0100

    chore: Add custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,15 @@ module.exports = {
   rules: {
     'import/prefer-default-export': 0,
     "arrow-parens": [ 'error', "as-needed" ],
+    'no-plusplus': 0,
+    'no-param-reassign': 0,
+    'no-mixed-operators': 0,
+    'no-continue': 0,
+    'no-console': 0,
+    'no-prototype-builtins': 0,
+    'no-nested-ternary': 0,
+    'default-case': 0,
+    'space-before-function-paren': 0,
+    'guard-for-in': 0,
   }
 }
