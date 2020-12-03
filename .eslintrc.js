0cd0108e56fc31085353c01e4c6db672a0d7e1e6
Jan Savli <jan.savli@gmail.com>
2020-12-03 11:40:11 +0100

    chore: Make prettier last lint extender

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
     'plugin:@stencil/recommended',
+    'plugin:prettier/recommended',
   ],
   env: {
     node: true,
