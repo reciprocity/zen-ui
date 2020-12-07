10aced150068343faf400d039e8236b9c420d100
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2020-12-07 10:35:11 -0300

    Updated tests

--- src/components/zen-checkbox/test/zen-checkbox.spec.tsx
+++ src/components/zen-checkbox/test/zen-checkbox.spec.tsx
@@ -15,4 +15,46 @@ describe('zen-checkbox', () => {
       </zen-checkbox>
     `);
   });
+
+  it('renders checked checkbox', async () => {
+    const page = await newSpecPage({
+      components: [ZenCheckbox],
+      html: `<zen-checkbox checked></zen-checkbox>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-checkbox checked="">
+        <mock:shadow-root>
+          <input checked="" class="input-control" type="checkbox">
+        </mock:shadow-root>
+      </zen-checkbox>
+    `);
+  });
+
+  it('renders disabled checkbox', async () => {
+    const page = await newSpecPage({
+      components: [ZenCheckbox],
+      html: `<zen-checkbox disabled></zen-checkbox>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-checkbox disabled="">
+        <mock:shadow-root>
+          <input class="input-control" type="checkbox" disabled="">
+        </mock:shadow-root>
+      </zen-checkbox>
+    `);
+  });
+
+  it('renders disabled and checked checkbox', async () => {
+    const page = await newSpecPage({
+      components: [ZenCheckbox],
+      html: `<zen-checkbox checked disabled></zen-checkbox>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-checkbox checked="" disabled="">
+        <mock:shadow-root>
+          <input checked="" class="input-control" type="checkbox" disabled="">
+        </mock:shadow-root>
+      </zen-checkbox>
+    `);
+  });
 });
