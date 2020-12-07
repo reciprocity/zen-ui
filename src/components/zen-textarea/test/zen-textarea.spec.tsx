99b0ab3390196747cf1fa9d73b884a462bc7560a
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2020-12-07 10:44:57 -0300

    Updated tests

--- src/components/zen-textarea/test/zen-textarea.spec.tsx
+++ src/components/zen-textarea/test/zen-textarea.spec.tsx
@@ -15,4 +15,74 @@ describe('zen-textarea', () => {
       </zen-textarea>
     `);
   });
+
+  it('renders with custom cols and rows', async () => {
+    const page = await newSpecPage({
+      components: [ZenTextarea],
+      html: `<zen-textarea cols="50" rows="15"></zen-textarea>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-textarea cols="50" rows="15">
+        <mock:shadow-root>
+          <textarea class="input-control" cols="50" rows="15"></textarea>
+        </mock:shadow-root>
+      </zen-textarea>
+    `);
+  });
+
+  it('renders with some custom value', async () => {
+    const page = await newSpecPage({
+      components: [ZenTextarea],
+      html: `<zen-textarea value="Something to test"></zen-textarea>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-textarea value="Something to test">
+        <mock:shadow-root>
+          <textarea class="input-control" cols="30" rows="5">Something to test</textarea>
+        </mock:shadow-root>
+      </zen-textarea>
+    `);
+  });
+
+  it('renders default with disabled property', async () => {
+    const page = await newSpecPage({
+      components: [ZenTextarea],
+      html: `<zen-textarea disabled></zen-textarea>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-textarea disabled="">
+        <mock:shadow-root>
+          <textarea class="input-control" cols="30" rows="5" disabled=""></textarea>
+        </mock:shadow-root>
+      </zen-textarea>
+    `);
+  });
+
+  it('renders default with required property', async () => {
+    const page = await newSpecPage({
+      components: [ZenTextarea],
+      html: `<zen-textarea required></zen-textarea>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-textarea required="">
+        <mock:shadow-root>
+          <textarea class="input-control" cols="30" rows="5" required=""></textarea>
+        </mock:shadow-root>
+      </zen-textarea>
+    `);
+  });
+
+  it('renders default with all custom properties', async () => {
+    const page = await newSpecPage({
+      components: [ZenTextarea],
+      html: `<zen-textarea disabled required value="Some custom value" rows="22" cols="44"></zen-textarea>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <zen-textarea disabled="" required="" value="Some custom value" rows="22" cols="44">
+        <mock:shadow-root>
+          <textarea class="input-control" cols="44" rows="22" disabled="" required="">Some custom value</textarea>
+        </mock:shadow-root>
+      </zen-textarea>
+    `);
+  });
 });
