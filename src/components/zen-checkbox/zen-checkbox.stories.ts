2333f31cf13f675fc5c07255acf5c072d83e049e
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2020-12-09 10:40:04 -0300

    Improved styles and stories structure

--- src/components/zen-checkbox/zen-checkbox.stories.ts
+++ src/components/zen-checkbox/zen-checkbox.stories.ts
@@ -1,45 +1,22 @@
 import { html } from 'lit-html';
 import markdown from './readme.md';
 
-const argTypes = {
-  label: {
-    defaultValue: 'Label',
-    control: {
-      type: 'text',
-    },
-  },
-  checked: {
-    control: {
-      type: 'boolean',
-    },
-  },
-  disabled: {
-    control: {
-      type: 'boolean',
-    },
-  },
-  required: {
-    control: {
-      type: 'boolean',
-    },
-  },
-};
-
 export default {
   title: 'Components/Zen Checkbox',
   component: 'zen-checkbox',
-  argTypes,
   parameters: {
     notes: { markdown },
   },
 };
 
 const Template = ({ label, checked, required, disabled }) => {
-  checked = checked ? checked : false;
-  disabled = disabled ? disabled : false;
-  required = required ? required : false;
-  label = label ? label : '';
   return html`<zen-checkbox checked=${checked} disabled=${disabled} label=${label} required=${required} />`;
 };
 
 export const Default = Template.bind({});
+Default.args = {
+  label: 'Label',
+  checked: false,
+  disabled: false,
+  required: false,
+};
