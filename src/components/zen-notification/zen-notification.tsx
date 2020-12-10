afe56a05c97f20fc838e274b810e0746e3efdb80
Mihael Palfi <mihael@reciprocitylabs.com>
2020-12-10 12:39:51 +0100

    Add notification implementation and styles

--- src/components/zen-notification/zen-notification.tsx
+++ src/components/zen-notification/zen-notification.tsx
@@ -1,4 +1,7 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop } from '@stencil/core';
+import { renderIcon } from '../helpers/fa-icons';
+import { faTimes } from '@fortawesome/pro-solid-svg-icons';
+import { getIcon, getTimeout, ZenDismissDuration, ZenVariant } from './zen-notification-helper';
 
 @Component({
   tag: 'zen-notification',
@@ -6,10 +9,78 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class ZenNotification {
-  render() {
+  div: HTMLElement = undefined;
+
+  /** Variant  */
+  @Prop() readonly variant: ZenVariant = ZenVariant.SUCCESS;
+
+  /** Height */
+  @Prop() readonly height: string = '5rem';
+
+  /** Width */
+  @Prop() readonly width: string = '25rem';
+
+  /** Title */
+  @Prop() readonly nTitle: string = '';
+
+  /** Message */
+  @Prop() readonly nMessage: string = '';
+
+  /** Hide duration */
+  @Prop() readonly dismissDuration: ZenDismissDuration = ZenDismissDuration.MEDIUM;
+
+  /** Can dismiss */
+  @Prop() readonly dismiss: boolean = false;
+
+  close(el) {
+    el.className = '';
+  }
+
+  componentDidRender() {
+    if (this.dismissDuration !== ZenDismissDuration.NONE) {
+      setTimeout(() => {
+        this.close(this.div);
+      }, getTimeout(this.dismissDuration));
+    }
+  }
+
+  render(): HTMLElement {
+    const classes = {
+      notification: true,
+      [`notification-${this.variant}`]: true,
+    };
+
+    const close = {
+      close: true,
+      [`close-${this.variant}`]: true,
+      hide: this.dismiss == false,
+    };
+
+    const icon = {
+      [`icon-${this.variant}`]: true,
+    };
+
     return (
-      <Host>
-        <slot></slot>
+      <Host class="show" ref={el => (this.div = el)}>
+        <div class={classes} style={{ width: this.width, 'min-height': this.height }}>
+          <div
+            class={close}
+            onClick={() => {
+              this.close(this.div);
+            }}
+          >
+            {renderIcon(faTimes)}
+          </div>
+          <div class="row">
+            <div class="icon-container">
+              <div class={icon}>{getIcon(this.variant)}</div>
+            </div>
+            <div class="content-container">
+              <div class="title">{this.nTitle}</div>
+              <div class="message">{this.nMessage}</div>
+            </div>
+          </div>
+        </div>
       </Host>
     );
   }
