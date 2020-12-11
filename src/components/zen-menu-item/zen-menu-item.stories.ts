c3754c5cabb6e6106b158a49fa3ad48125dfae01
Jan Savli <jan.savli@gmail.com>
2020-12-11 12:09:38 +0100

    feat: add story for custom slot

--- src/components/zen-menu-item/zen-menu-item.stories.ts
+++ src/components/zen-menu-item/zen-menu-item.stories.ts
@@ -12,12 +12,21 @@ export default {
   },
 };
 
-const Template = ({ selected, label }) => {
-  return html` <zen-menu-item selected=${selected} label=${label}></zen-menu-item> `;
+const Template = ({ selected, label, focused }) => {
+  return html` <zen-menu-item ?selected=${selected} ?focused=${focused} label=${label} />`;
 };
 
 export const Default = Template.bind({});
 Default.args = {
   selected: false,
+  focused: false,
   label: 'My menu item',
 };
+
+const SlottedTemplate = () => {
+  return html` <zen-menu-item>
+    <div slot="content">Custom content</div>
+  </zen-menu-item>`;
+};
+
+export const CustomContentSlot = SlottedTemplate.bind({});
