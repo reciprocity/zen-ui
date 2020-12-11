8a8c9b374efc917d1178df6d5a3f86b77bf981fe
Jan Savli <jan.savli@gmail.com>
2020-12-11 10:16:51 +0100

    refactor: extract functions slotPassed and getSlotElement

--- src/components/helpers/helpers.ts
+++ src/components/helpers/helpers.ts
@@ -7,3 +7,16 @@ export function waitNextFrame(): Promise<boolean> {
     window.requestAnimationFrame(() => resolve());
   });
 }
+
+export function slotPassed(host: HTMLElement, slotName: string): boolean {
+  const selector = slotName ? `slot[name="${slotName}"]` : 'slot';
+  return !!host.querySelector(selector);
+}
+
+export function getSlotElement(host: HTMLElement, slotName: string): HTMLElement | undefined {
+  // Note: Don't use this function to determine if slot was passed by consumer!
+  const selector = slotName ? `slot[name="${slotName}"]` : 'slot';
+  const slot = host.shadowRoot.querySelector(selector) as HTMLSlotElement;
+  if (!slot) return undefined;
+  return slot.assignedNodes()[0] as HTMLElement;
+}
