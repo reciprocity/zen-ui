35bcbe8a02d1e97b4b33ea0122ee3e060e0d1c6a
Mihael Palfi <mihael@reciprocitylabs.com>
2020-12-14 11:03:59 +0100

    Update and add additional stories

--- src/components/zen-tooltip/zen-tooltip.stories.ts
+++ src/components/zen-tooltip/zen-tooltip.stories.ts
@@ -1,6 +1,9 @@
 import { html } from 'lit-html';
 import markdown from './readme.md';
 import { Position, Variant } from './types';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/pro-light-svg-icons';
+library.add(faTrash);
 
 const argTypes = {
   position: {
@@ -25,42 +28,54 @@ export default {
 
 const Template = ({ position, text, variant }) => {
   return html`
-    <div style="display: block;text-align: center; padding-top: 10rem; padding-bottom: 10rem">
-      <label>Tooltip</label>
-      <zen-tooltip position="${position}" variant="${variant}" text="${text}"> </zen-tooltip>
+    <div style="text-align: center; margin-top: 8rem; margin-bottom: 8rem;">
+      <zen-button label="Apply changes"></zen-button>
+      <zen-tooltip position="${position}" variant="${variant}" text="${text}"></zen-tooltip>
     </div>
   `;
 };
 
 const TemplateSlot = () => {
   return html`
-    <label style="cursor: pointer;">Tooltip</label>
-    <zen-tooltip position="bottom" text="Override text">
-      <div slot="text"><b>This is a HTML content</b></div>
-    </zen-tooltip>
+    <div style="text-align: center; margin-top: 8rem;">
+      <div>Tooltip</div>
+      <zen-tooltip position="top" variant="light" text="Override text">
+        <div slot="text"><b>This is a HTML content</b></div>
+      </zen-tooltip>
+    </div>
   `;
 };
 
-const TemplateAlert = () => {
+const TemplateVariants = () => {
   return html`
-    <label>Tooltip</label>
-    <zen-tooltip
-      position="bottom"
-      variant="error"
-      text="The Manager can edit and create in the context of the PCI-DSS framework. They can also grant access to the
-        framework to additional teammates."
-    >
-    </zen-tooltip>
+    <table style="width: 100%; height: 400px; table-layout: fixed; ">
+      <tr>
+        <td style="text-align: center;">
+          <zen-button label="Edit" variant="primary"></zen-button>
+          <zen-tooltip position="top" text="The Manager can edit and create in the context of the PCI-DSS framework.">
+          </zen-tooltip>
+        </td>
+        <td style="text-align: center;">
+          <zen-button label="Info" variant="secondary"></zen-button>
+          <zen-tooltip position="top" variant="light" text="This is a information tooltip."></zen-tooltip>
+        </td>
+        <td style="text-align: center;">
+          <zen-button label="Delete" variant="destructive"></zen-button>
+          <zen-tooltip position="top" variant="error" text="Are you sure you want to delete the object!"></zen-tooltip>
+        </td>
+      </tr>
+    </table>
   `;
 };
 
 export const Default = Template.bind({});
 Default.args = {
   position: Position.TOP,
-  variant: Variant.DEFAULT,
+  variant: Variant.DARK,
   text:
     'The Manager can edit and create in the context of the PCI-DSS framework. They can also grant access to the framework to additional teammates.',
+  offset: 10,
 };
 
 export const TooltipWithHTMLContent = TemplateSlot.bind({});
-export const TooltipVariantAlert = TemplateAlert.bind({});
+export const TooltipVariants = TemplateVariants.bind({});
