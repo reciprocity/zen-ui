b0c021b4fa0d9842536e0a1a9feba4a132764036
Jan Savli <jan.savli@gmail.com>
2021-01-06 16:09:38 +0100

    test: poc for notification component visual test

--- src/components/zen-notification/test/zen-notification.e2e.ts
+++ src/components/zen-notification/test/zen-notification.e2e.ts
@@ -1,4 +1,4 @@
-import { newE2EPage } from '@stencil/core/testing';
+import { newE2EPage, E2EPage } from '@stencil/core/testing';
 
 describe('zen-notification', () => {
   it('renders', async () => {
@@ -7,5 +7,19 @@ describe('zen-notification', () => {
 
     const element = await page.find('zen-notification');
     expect(element).toHaveClass('hydrated');
+  });
+
+  it('default render', async () => {
+    const page: E2EPage = await newE2EPage();
+    await page.setContent('<zen-notification variant="error"></zen-notification>');
+
+    // To start comparing the visual result, you first must run page.compareScreenshot; This will capture a screenshot, and save the file to "/screenshot/images". You'll be able to check that into your repo to provide those results to your team. You can only have one of these commands per test:
+    const results = await page.compareScreenshot();
+
+    // Finally, we can test against the previous screenshots.
+    // Test against hard pixels
+    expect(results).toMatchScreenshot({ allowableMismatchedPixels: 100 });
+    // Test against the percentage of changes. if 'allowableMismatchedRatio' is above 20% changed
+    expect(results).toMatchScreenshot({ allowableMismatchedRatio: 0.2 });
   });
 });
