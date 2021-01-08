797c591812feab5aeed68ea1eb5439b57d8c1b26
Matic Plešec <m.plesec@gmail.com>
2021-01-08 13:34:52 +0100

    refactor: move toInlineFrameContentUrl to utils

--- cypress/integration/html-playground.e2e.js
+++ cypress/integration/html-playground.e2e.js
@@ -1,16 +1,6 @@
 /// <reference types="cypress" />
 
-import { storybookUrl } from '../constants';
-
-// TODO: TEMP move this to global scope
-/**
- * Returns URL for iframe content of a given Storybook docs page
- *
- * @param {string} id  Storybook docs page id
- */
-function toInlineFrameContentUrl(id) {
-  return `${storybookUrl}/iframe.html?id=${id}&viewMode=docs`;
-}
+import { toInlineFrameContentUrl } from '../utils';
 
 context('HTML Playground', () => {
   const pageId = 'playground--page';
