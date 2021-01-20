5a70a49a851cdbdb698a58c92f52741982258ddb
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2021-01-20 14:25:08 -0300

    test: updated zen-label tests

--- src/components/zen-label/test/zen-label.spec.tsx
+++ src/components/zen-label/test/zen-label.spec.tsx
@@ -2,19 +2,30 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ZenLabel } from '../zen-label';
 
 describe('zen-label', () => {
-  it('renders', async () => {
+  it('should render with shadow dom', async () => {
     const page = await newSpecPage({
       components: [ZenLabel],
       html: `<zen-label></zen-label>`,
     });
-    expect(page.root).toEqualHtml(`
-      <zen-label>
-        <mock:shadow-root>
-          <label>
-            Label
-          </label>
-        </mock:shadow-root>
-      </zen-label>
-    `);
+    expect(page.root.shadowRoot).toBeTruthy();
+  });
+
+  it('should render label', async () => {
+    const page = await newSpecPage({
+      components: [ZenLabel],
+      html: `<zen-label></zen-label>`,
+    });
+    expect(page.root.shadowRoot.querySelector('label')).toBeTruthy();
+    expect(page.root.shadowRoot.querySelector('label').innerText).toBe('Label');
+  });
+
+  it('should render required', async () => {
+    const page = await newSpecPage({
+      components: [ZenLabel],
+      html: `<zen-label required></zen-label>`,
+    });
+    expect(page.root.shadowRoot.querySelector('label')).toBeTruthy();
+    expect(page.root.shadowRoot.querySelector('label').innerText).toBe('Label*');
+    expect(page.root.shadowRoot.querySelector('span').classList.contains('required')).toBe(true);
   });
 });
