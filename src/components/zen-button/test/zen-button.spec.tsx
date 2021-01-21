6c0c3c3274f7ff1ec0819110dab7d7eaff55cb0b
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2021-01-21 08:57:42 -0300

    test: updated zen-checkbox and zen-button tests

--- src/components/zen-button/test/zen-button.spec.tsx
+++ src/components/zen-button/test/zen-button.spec.tsx
@@ -10,54 +10,18 @@ describe('zen-button variants rendering', () => {
     expect(page.root.shadowRoot).toBeTruthy();
   });
 
-  it.each(['primary', 'secondary', 'tertiary', 'positive', 'destructive'])('renders %s button', async variant => {
-    const page = await newSpecPage({
-      components: [ZenButton],
-      html: `<zen-button variant="${variant}"></zen-button>`,
-    });
-    expect(page.root.classList.contains(`btn-${variant}`)).toBe(true);
-  });
-
-  it('renders with loading state', async () => {
-    const page = await newSpecPage({
-      components: [ZenButton],
-      html: `<zen-button loading></zen-button>`,
-    });
-    expect(page.root.shadowRoot.querySelector('zen-spinner')).toBeTruthy();
-  });
-
-  it('renders with disabled state', async () => {
-    const page = await newSpecPage({
-      components: [ZenButton],
-      html: `<zen-button disabled></zen-button>`,
-    });
-    expect(page.root.classList.contains('disabled')).toBe(true);
-  });
-
-  it('renders with leadingIcon slot fulfilled', async () => {
-    const page = await newSpecPage({
-      components: [ZenButton],
-      html: `<zen-button>Button<zen-spinner slot="leadingIcon"></zen-spinner></zen-button>`,
-    });
-    expect(page.root.querySelector('[slot="leadingIcon"]')).toBeTruthy();
-  });
-
-  it('renders with trailingIcon slot fulfilled', async () => {
-    const page = await newSpecPage({
-      components: [ZenButton],
-      html: `<zen-button>Button<zen-spinner slot="trailingIcon"></zen-spinner></zen-button>`,
-    });
-    expect(page.root.querySelector('[slot="trailingIcon"]')).toBeTruthy();
-  });
-
-  it('renders with leadingIcon and trailingIcon slot fulfilled', async () => {
-    const page = await newSpecPage({
-      components: [ZenButton],
-      html: `<zen-button>Button<zen-spinner slot="leadingIcon"></zen-spinner><zen-spinner slot="trailingIcon"></zen-spinner></zen-button>`,
-    });
-    expect(page.root.querySelector('[slot="leadingIcon"]')).toBeTruthy();
-    expect(page.root.querySelector('[slot="trailingIcon"]')).toBeTruthy();
-  });
+  it.each(['primary', 'secondary', 'tertiary', 'positive', 'destructive'])(
+    'renders %s button with loading and disabled props',
+    async variant => {
+      const page = await newSpecPage({
+        components: [ZenButton],
+        html: `<zen-button variant="${variant}" loading disabled></zen-button>`,
+      });
+      expect(page.root.classList.contains(`btn-${variant}`)).toBe(true);
+      expect(page.root.classList.contains('disabled')).toBe(true);
+      expect(page.root.shadowRoot.querySelector('zen-spinner')).toBeTruthy();
+    },
+  );
 
   it('triggers click event on onKeyDown Enter key', async () => {
     const page = await newSpecPage({
--- src/components/zen-checkbox/test/zen-checkbox.spec.tsx
+++ src/components/zen-checkbox/test/zen-checkbox.spec.tsx
@@ -10,77 +10,23 @@ describe('zen-checkbox', () => {
     expect(page.root.shadowRoot).toBeTruthy();
   });
 
-  it('should render unchecked', async () => {
+  it('should render with disabled, required and checked props in', async () => {
     const page = await newSpecPage({
       components: [ZenCheckbox],
-      html: `<zen-checkbox></zen-checkbox>`,
-    });
-    expect(page.root.getAttribute('disabled')).toBeFalsy();
-    expect(page.root.getAttribute('required')).toBeFalsy();
-    expect(page.root.getAttribute('checked')).toBeFalsy();
-  });
-
-  it('should render checked', async () => {
-    const page = await newSpecPage({
-      components: [ZenCheckbox],
-      html: `<zen-checkbox checked></zen-checkbox>`,
-    });
-    expect(page.root.getAttribute('disabled')).toBeFalsy();
-    expect(page.root.getAttribute('required')).toBeFalsy();
-    expect(page.root.getAttribute('checked')).toBe('');
-  });
-
-  it('should render disabled', async () => {
-    const page = await newSpecPage({
-      components: [ZenCheckbox],
-      html: `<zen-checkbox disabled></zen-checkbox>`,
+      html: `<zen-checkbox checked disabled required></zen-checkbox>`,
     });
     expect(page.root.getAttribute('disabled')).toBe('');
-    expect(page.root.getAttribute('required')).toBeFalsy();
-    expect(page.root.getAttribute('checked')).toBeFalsy();
-  });
-
-  it('should render disabled and checked', async () => {
-    const page = await newSpecPage({
-      components: [ZenCheckbox],
-      html: `<zen-checkbox checked disabled></zen-checkbox>`,
-    });
-    expect(page.root.getAttribute('disabled')).toBe('');
-    expect(page.root.getAttribute('required')).toBeFalsy();
+    expect(page.root.getAttribute('required')).toBe('');
     expect(page.root.getAttribute('checked')).toBe('');
   });
 
   it('should render with label', async () => {
     const page = await newSpecPage({
       components: [ZenCheckbox],
-      html: `<zen-checkbox>This is an example label</zen-checkbox>`,
+      html: `<zen-checkbox label="This is an example label"></zen-checkbox>`,
     });
-    expect(page.root.getAttribute('disabled')).toBeFalsy();
-    expect(page.root.getAttribute('required')).toBeFalsy();
-    expect(page.root.getAttribute('checked')).toBeFalsy();
     expect(page.root.shadowRoot.querySelector('label')).toBeTruthy();
-  });
-
-  it('should render with label and disabled', async () => {
-    const page = await newSpecPage({
-      components: [ZenCheckbox],
-      html: `<zen-checkbox disabled="">This is an example label</zen-checkbox>`,
-    });
-    expect(page.root.getAttribute('disabled')).toBe('');
-    expect(page.root.getAttribute('required')).toBeFalsy();
-    expect(page.root.getAttribute('checked')).toBeFalsy();
-    expect(page.root.shadowRoot.querySelector('label')).toBeTruthy();
-  });
-
-  it('should render with label and required', async () => {
-    const page = await newSpecPage({
-      components: [ZenCheckbox],
-      html: `<zen-checkbox required="">This is an example label</zen-checkbox>`,
-    });
-    expect(page.root.getAttribute('disabled')).toBeFalsy();
-    expect(page.root.getAttribute('required')).toBe('');
-    expect(page.root.getAttribute('checked')).toBeFalsy();
-    expect(page.root.shadowRoot.querySelector('label')).toBeTruthy();
+    expect(page.root.shadowRoot.querySelector('label').innerText).toBe('This is an example label ');
   });
 
   it('checks', async () => {
