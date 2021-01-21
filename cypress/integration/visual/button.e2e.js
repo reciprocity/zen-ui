839de5aea1aecae89b3d90e89bbb32b372485caf
Jan Savli <jan.savli@gmail.com>
2021-01-21 11:20:30 +0100

    tests: update buttons screenshots

--- cypress/integration/visual/button.e2e.js
+++ cypress/integration/visual/button.e2e.js
@@ -4,12 +4,10 @@ import {createVisualTests} from '../../support/utils/visualTesting';
 describe('Button visual tests', () => {
 
   const pageId = 'buttons-button--default';
-  const stories = ["story--buttons-button--story-simple", 
-  "story--buttons-button--story-loading", 
-  "story--buttons-button--story-disabled",  
+  const stories = ["story--buttons-button--story-simple",
+  "story--buttons-button--story-loading",
+  "story--buttons-button--story-disabled",
   "story--buttons-button--story-with-leading-slot",
-  "story--buttons-button--story-with-trailing-slot",
-  "story--buttons-button--story-with-leading-and-trailing-slots", 
   "story--buttons-button--default-story"];
 
   // Example how to skip testing for some user stories
