09f383810d501929f9cda860b6cc9ddc549a54cf
Mihael Palfi <mihael@reciprocitylabs.com>
2021-01-22 08:25:11 +0100

    Fix visual regression tests

--- cypress/integration/visual/avatar.e2e.js
+++ cypress/integration/visual/avatar.e2e.js
@@ -4,8 +4,9 @@ import {createVisualTests} from '../../support/utils/visualTesting';
 describe('Avatar visual tests', () => {
 
   const pageId = 'icons-avatar--default';
-  const stories = ["story--icons-avatar--story-variants",
-  "story--icons-avatar--story-sizes"];
+  const stories = ["story--icons-avatar--story-avatar-variants",
+  "story--icons-avatar--story-avatar-sizes",
+  "story--icons-avatar--default-story"];
 
   // Example how to skip testing for some user stories
   const skipedStories = [];
