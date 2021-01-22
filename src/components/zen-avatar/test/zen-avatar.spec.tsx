adad9e812da63017fa590131b281ba74baebd3fe
Mihael Palfi <mihael@reciprocitylabs.com>
2021-01-22 07:32:58 +0100

    Add unit tests

--- src/components/zen-avatar/test/zen-avatar.spec.tsx
+++ src/components/zen-avatar/test/zen-avatar.spec.tsx
@@ -17,3 +17,33 @@ describe('zen-avatar', () => {
     `);
   });
 });
+
+describe('Test parameters rendering', () => {
+  it.each(['medium', 'large'])('Test that size %s is applied correctly', async size => {
+    const page = await newSpecPage({
+      components: [ZenAvatar],
+      html: `<zen-avatar size="${size}" label="MP"></zen-avatar>`,
+    });
+    expect(page.root.classList.contains(size)).toBe(true);
+  });
+
+  it('Test that image is hidden and label is displayed', async () => {
+    const page = await newSpecPage({
+      components: [ZenAvatar],
+      html: `<zen-avatar label="MP"></zen-avatar>`,
+    });
+    expect(page.root.shadowRoot.querySelector('div').textContent).toEqual('MP');
+    expect(page.root.shadowRoot.querySelector('img').classList.contains('hidden')).toBe(true);
+    expect(page.root.shadowRoot.querySelector('div').classList.contains('hidden')).toBe(false);
+  });
+
+  it('Test that image is shown and label hidden', async () => {
+    const page = await newSpecPage({
+      components: [ZenAvatar],
+      html: `<zen-avatar image-url="img.jpg"></zen-avatar>`,
+    });
+    expect(page.root.shadowRoot.querySelector('div').textContent).toEqual('');
+    expect(page.root.shadowRoot.querySelector('img').classList.contains('hidden')).toBe(false);
+    expect(page.root.shadowRoot.querySelector('div').classList.contains('hidden')).toBe(true);
+  });
+});
