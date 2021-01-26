fb4b31bf03a42bf6b389d914f518bce27a68e0b2
Matic Plešec <m.plesec@gmail.com>
2021-01-26 17:22:25 +0100

    fix: use scss file for ZenTableRow styles

--- src/components/zen-table-row/zen-table-row.tsx
+++ src/components/zen-table-row/zen-table-row.tsx
@@ -2,7 +2,7 @@ import { Component, Host, h } from '@stencil/core';
 
 @Component({
   tag: 'zen-table-row',
-  styleUrl: 'zen-table-row.css',
+  styleUrl: 'zen-table-row.scss',
   shadow: true,
 })
 export class ZenTableRow {
