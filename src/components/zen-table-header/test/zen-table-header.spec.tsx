ae5c519173c54fa8e30d3da57d4289062acdc850
Matic Plešec <m.plesec@gmail.com>
2021-01-27 15:58:56 +0100

    feature: add ZenTableHeader component

---
+++ src/components/zen-table-header/test/zen-table-header.spec.tsx
@@ -0,0 +1,8 @@
+// import { newSpecPage } from '@stencil/core/testing';
+// import { ZenTableHeader } from '../zen-table-header';
+
+describe('zen-table-header', () => {
+  it('renders', async () => {
+    // TODO:
+  });
+});
