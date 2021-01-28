3c9923db0c3b5034753511c54f1239437468fb49
Matic Plešec <m.plesec@gmail.com>
2021-01-28 14:00:28 +0100

    test: add unit tests for ZenTable

--- src/components/zen-table/test/zen-table.spec.tsx
+++ src/components/zen-table/test/zen-table.spec.tsx
@@ -1,8 +1,21 @@
-// import { newSpecPage } from '@stencil/core/testing';
-// import { ZenTable } from '../zen-table';
+import { newSpecPage } from '@stencil/core/testing';
+import { ZenTable } from '../zen-table';
 
 describe('zen-table', () => {
   it('renders', async () => {
-    // TODO:
+    const page = await newSpecPage({
+      components: [ZenTable],
+      html: `<zen-table>Content</zen-table>`,
+    });
+
+    expect(page.root).toEqualHtml(`
+      <zen-table>
+        <mock:shadow-root>
+          <slot>
+          </slot>
+        </mock:shadow-root>
+        Content
+      </zen-table>
+    `);
   });
 });
