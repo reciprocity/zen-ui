8ef195405c971c34e0391b2a849978ae4416dc6a
Jan Savli <jan.savli@gmail.com>
2021-01-29 21:14:04 +0100

    tests: update vrt

--- cypress/integration/visual/input.e2e.js
+++ cypress/integration/visual/input.e2e.js
@@ -3,14 +3,13 @@ import {createVisualTests} from '../../support/utils/visualTesting';
 
 describe('Input visual tests', () => {
 
-  const pageId = 'forms-input--story-simple';
-  const stories = ["story--forms-input--story-simple", 
-  "story--forms-input--story-invalid", 
-  "story--forms-input--story-disabled", 
-  "story--forms-input--story-with-leading-slot", 
-  "story--forms-input--story-with-trailing-slot", 
-  "story--forms-input--story-with-leading-and-trailing-slots", 
-  "story--forms-input--default-story"];
+  const pageId = 'forms-input';
+  const stories = [
+    'story--forms-input--story-with-leading-slot',
+    'story--forms-input--story-with-trailing-slot',
+    'story--forms-input--story-with-leading-and-trailing-slots',
+    'story--forms-input--default-story'
+  ];
 
   before(() => {
     cy.visitStorybookIframe(pageId);
