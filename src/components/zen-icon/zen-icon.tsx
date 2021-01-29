ddcd95b1abc5dceb97d322fa76ab1be4a430b65d
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2021-01-29 09:08:41 -0300

    refactor: updated zen-icon logic and stories

--- src/components/zen-icon/zen-icon.tsx
+++ src/components/zen-icon/zen-icon.tsx
@@ -1,5 +1,8 @@
 import { Component, Host, h, Prop, Element } from '@stencil/core';
 
+import { renderIcon } from '../helpers/fa-icons';
+import { IconDefinition } from '@fortawesome/fontawesome-common-types';
+
 @Component({
   tag: 'zen-icon',
   styleUrl: 'zen-icon.scss',
@@ -11,11 +14,10 @@ export class ZenIcon {
   /** Size of the icon. */
   @Prop() readonly size: string = '';
 
+  /** Icon to be rendered. */
+  @Prop() readonly icon: IconDefinition;
+
   render(): HTMLElement {
-    return (
-      <Host class={{ lg: this.size === 'lg' }}>
-        <slot></slot>
-      </Host>
-    );
+    return <Host class={{ lg: this.size === 'lg' }}>{renderIcon(this.icon)}</Host>;
   }
 }
