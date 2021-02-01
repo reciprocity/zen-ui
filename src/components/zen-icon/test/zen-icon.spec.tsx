c9ac22b23afe70d2ab0c5a15a2f984f96509883f
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2021-02-01 09:00:12 -0300

    test: removed unnecessary test from zen-icon

--- src/components/zen-icon/test/zen-icon.spec.tsx
+++ src/components/zen-icon/test/zen-icon.spec.tsx
@@ -1,6 +1,5 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { ZenIcon } from '../zen-icon';
-import { faUser } from '@fortawesome/pro-light-svg-icons';
 
 describe('zen-icon', () => {
   it('should render with shadow dom', async () => {
@@ -17,16 +16,5 @@ describe('zen-icon', () => {
       html: `<zen-icon></zen-icon>`,
     });
     expect(page.root.innerHTML).toBe('');
-  });
-
-  it('should render a boxed icon', async () => {
-    const page = await newSpecPage({
-      components: [ZenIcon],
-      html: `<zen-icon></zen-icon>`,
-    });
-    page.rootInstance.icon = faUser;
-    page.waitForChanges();
-    expect(page.rootInstance.icon.prefix).toBe('fal');
-    expect(page.rootInstance.icon.iconName).toBe('user');
   });
 });
