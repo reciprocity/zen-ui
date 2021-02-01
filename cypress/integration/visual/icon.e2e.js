7fd95a77aec3bd661de325057e97e800bfb5e47a
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2021-02-01 02:49:56 -0300

    test: added vrt for zen-icon and updated others

--- cypress/integration/visual/icon.e2e.js
+++ cypress/integration/visual/icon.e2e.js
@@ -1,15 +1,9 @@
 /// <reference types="cypress" />
 import { createVisualTests } from '../../support/utils/visualTesting';
 
-describe('Button visual tests', () => {
-  const pageId = 'buttons-button--default';
-  const stories = [
-    'story--buttons-button--story-simple',
-    'story--buttons-button--story-loading',
-    'story--buttons-button--story-disabled',
-    'story--buttons-button--story-with-icons',
-    'story--buttons-button--default-story',
-  ];
+describe('Icon visual tests', () => {
+  const pageId = 'icons-icon--default';
+  const stories = ['story--icons-icon--default-story'];
 
   // Example how to skip testing for some user stories
   const skipedStories = [];
