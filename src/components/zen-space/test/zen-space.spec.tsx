ab1a0b70f601906285763909daca77fb6ca316f7
Jan Savli <jan.savli@gmail.com>
2021-02-02 09:18:57 +0100

    tests: add unit tests

--- src/components/zen-space/test/zen-space.spec.tsx
+++ src/components/zen-space/test/zen-space.spec.tsx
@@ -1,20 +1,45 @@
 import { newSpecPage } from '@stencil/core/testing';
-import { ZenSpinner } from '../zen-spinner';
+import { ZenSpace } from '../zen-space';
 
-describe('zen-spinner', () => {
+describe('zen-space', () => {
   it('should render with shadow dom', async () => {
     const page = await newSpecPage({
-      components: [ZenSpinner],
-      html: `<zen-spinner></zen-spinner>`,
+      components: [ZenSpace],
+      html: `<zen-space></zen-space>`,
     });
     expect(page.root.shadowRoot).toBeTruthy();
   });
 
-  it('renders', async () => {
+  it('correctly parses padding shorthands', async () => {
     const page = await newSpecPage({
-      components: [ZenSpinner],
-      html: `<zen-spinner></zen-spinner>`,
+      components: [ZenSpace],
+      html: `<zen-space padding="xs sm"></zen-space>`,
     });
-    expect(page.root.shadowRoot.querySelector('span').classList.contains('spinner')).toBe(true);
+    const space = page.root;
+
+    expect(space.classList.contains('padding-x-sm')).toBe(true);
+    expect(space.classList.contains('padding-y-xs')).toBe(true);
+
+    space.padding = 'md';
+    await page.waitForChanges();
+    expect(space.classList.contains('padding-md')).toBe(true);
+
+    space.padding = 'lg xl';
+    await page.waitForChanges();
+    expect(space.classList.contains('padding-x-xl')).toBe(true);
+    expect(space.classList.contains('padding-y-lg')).toBe(true);
+
+    space.padding = 'sm lg xl';
+    await page.waitForChanges();
+    expect(space.classList.contains('padding-x-lg')).toBe(true);
+    expect(space.classList.contains('padding-top-sm')).toBe(true);
+    expect(space.classList.contains('padding-bottom-xl')).toBe(true);
+
+    space.padding = 'none xs sm md';
+    await page.waitForChanges();
+    expect(space.classList.contains('padding-top-none')).toBe(true);
+    expect(space.classList.contains('padding-right-xs')).toBe(true);
+    expect(space.classList.contains('padding-bottom-sm')).toBe(true);
+    expect(space.classList.contains('padding-left-md')).toBe(true);
   });
 });
