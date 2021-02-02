976c8c79aa0f743c0d9d6266b375e06ae923fd1b
Jan Savli <jan.savli@gmail.com>
2021-02-02 09:27:38 +0100

    tests: add vr tests

---
+++ cypress/integration/visual/space.e2e.js
@@ -0,0 +1,23 @@
+/// <reference types='cypress' />
+import {createVisualTests} from '../../support/utils/visualTesting';
+
+describe('Button visual tests', () => {
+
+  const pageId = 'layout-space';
+  const stories = ['story--layout-space--sizes',
+    'story--layout-space--aligns',
+    'story--layout-space--aligns-column',
+    'story--layout-space--paddings',
+    'story--layout-space--default-story',
+  ]
+
+  // Example how to skip testing for some user stories
+  const skipedStories = [];
+
+  before(() => {
+    cy.visitStorybookIframe(pageId);
+    cy.verifyAllStoriesHaveVRT(stories, skipedStories);
+  });
+
+  createVisualTests(stories, skipedStories)
+});
