33d3efb568d2504e61b760142a65bfb1a336ad41
Jan Savli <jan.savli@gmail.com>
2021-02-03 15:12:24 +0100

    feature: manually deselect other radios in same group
    it doesn't work out of the box, because we're in a shadow dom and radios don't see each other

--- src/components/zen-radio/zen-radio.tsx
+++ src/components/zen-radio/zen-radio.tsx
@@ -1,4 +1,5 @@
 import { Component, Host, h, Prop } from '@stencil/core';
+import { querySelectorAllDeep } from 'query-selector-shadow-dom';
 
 @Component({
   tag: 'zen-radio',
@@ -9,10 +10,30 @@ export class ZenRadio {
   /** Shows a red asterisk after label. */
   @Prop() readonly required = false;
 
+  /** Group id to which this radio belongs */
+  @Prop() readonly group: string = '';
+
+  onClick(event: Event): void {
+    // Since we're in a shadow dom, we have to manually deselect all other
+    //   radios of the same group:
+    const radios = querySelectorAllDeep(`input[name=${this.group}]`);
+    Array.from(radios)
+      .filter(radio => radio !== event.target)
+      .forEach((radio: HTMLInputElement) => {
+        radio.checked = false;
+      });
+  }
+
   render(): HTMLElement {
     return (
       <Host>
-        <input type="radio" class="input-control" id="radio" />
+        <input
+          type="radio"
+          class="input-control"
+          id="radio"
+          name={this.group}
+          onChange={event => this.onClick(event)}
+        />
         <label htmlFor="radio">
           <slot />
         </label>
