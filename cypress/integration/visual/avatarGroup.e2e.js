7917f8c9ccf3359981636fe68dc1018ff118e7e8
Mihael Palfi <mihael@reciprocitylabs.com>
2021-02-05 09:44:22 +0100

    Add avatar group visual regression test

---
+++ cypress/integration/visual/avatarGroup.e2e.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+import {createVisualTests} from '../../support/utils/visualTesting';
+
+describe('Avatar group visual tests', () => {
+
+  const pageId = 'icons-avatar-group--default';
+  const stories = ["story--icons-avatar-group--story-avatar-group",
+  "story--icons-avatar-group--default-story"];
+
+  // Example how to skip testing for some user stories
+  const skipedStories = [];
+
+  before(() => {
+    cy.visitStorybookIframe(pageId);
+    cy.verifyAllStoriesHaveVRT(stories, skipedStories);
+  });
+
+  createVisualTests(stories, skipedStories)
+});
