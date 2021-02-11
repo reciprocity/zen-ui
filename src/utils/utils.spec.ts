a7502d67880f430dd95b038e0443eedc801eabb0
Matic Plešec <m.plesec@gmail.com>
2021-02-11 14:04:45 +0100

    refactor: update utils tests descriptions

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,19 +1,19 @@
 import { format } from './utils';
 
 describe('format', () => {
-  it('returns empty string for no names defined', () => {
+  it('should return empty string if no names defined', () => {
     expect(format(undefined, undefined, undefined)).toEqual('');
   });
 
-  it('formats just first names', () => {
+  it('should format only first name', () => {
     expect(format('Joseph', undefined, undefined)).toEqual('Joseph');
   });
 
-  it('formats first and last names', () => {
+  it('should format first and last name', () => {
     expect(format('Joseph', undefined, 'Publique')).toEqual('Joseph Publique');
   });
 
-  it('formats first, middle and last names', () => {
+  it('should format first, middle, and last name', () => {
     expect(format('Joseph', 'Quincy', 'Publique')).toEqual('Joseph Quincy Publique');
   });
 });
