924d3dafdc7abaa1f5d7c36bcff99124912b7c0a
Matic Plešec <m.plesec@gmail.com>
2021-02-12 13:15:15 +0100

    build: split eslint configs between source and cypress tests

--- cypress/.eslintrc.js
+++ cypress/.eslintrc.js
@@ -1,6 +1,16 @@
-{
-  "extends": [
-    "plugin:cypress/recommended",
-    "plugin:chai-friendly/recommended",
-  ]
-}
+module.exports = {
+  plugins: ['cypress'],
+  extends: [
+    'eslint:recommended',
+    'plugin:cypress/recommended',
+    'plugin:chai-friendly/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    node: true,
+    'cypress/globals': true,
+  },
+  globals: {
+    cy: true,
+  },
+};
