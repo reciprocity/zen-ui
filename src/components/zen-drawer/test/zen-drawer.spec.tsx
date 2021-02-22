eb8c5a83e357f7c3a224f29657a10285f63e41cb
Mihael Palfi <mihael@reciprocitylabs.com>
2021-02-22 16:12:45 +0100

    Add test case

--- src/components/zen-drawer/test/zen-drawer.spec.tsx
+++ src/components/zen-drawer/test/zen-drawer.spec.tsx
@@ -2,17 +2,11 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ZenDrawer } from '../zen-drawer';
 
 describe('zen-drawer', () => {
-  it('renders', async () => {
+  it('should render with shadow dom', async () => {
     const page = await newSpecPage({
       components: [ZenDrawer],
       html: `<zen-drawer></zen-drawer>`,
     });
-    expect(page.root).toEqualHtml(`
-      <zen-drawer>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </zen-drawer>
-    `);
+    expect(page.root.shadowRoot).toBeTruthy();
   });
 });
