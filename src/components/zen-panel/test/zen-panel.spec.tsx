9d33958c4ed9b3c078355fb7e101dbba19778d78
Matic Plešec <m.plesec@gmail.com>
2021-02-22 12:00:53 +0100

    test: add ZenPanel unit tests

--- src/components/zen-panel/test/zen-panel.spec.tsx
+++ src/components/zen-panel/test/zen-panel.spec.tsx
@@ -10,4 +10,51 @@ describe('zen-panel', () => {
     });
     expect(page.root.shadowRoot).toBeTruthy();
   });
+
+  describe('icon', () => {
+    it('should display right chevron icon if hidden', () => {
+      const panel = new ZenPanel();
+
+      expect(panel.hidden).toBe(true);
+      expect(panel.icon().iconName).toEqual('chevron-right');
+    });
+
+    it('should display down chevron icon if not hidden', () => {
+      const panel = new ZenPanel();
+
+      panel.toggleHidden();
+
+      expect(panel.hidden).toBe(false);
+      expect(panel.icon().iconName).toEqual('chevron-down');
+    });
+  });
+
+  describe('content', () => {
+    it('should hide content on init', () => {
+      const panel = new ZenPanel();
+      expect(panel.hidden).toBe(true);
+    });
+
+    it('should not display content if hidden', () => {
+      const panel = new ZenPanel();
+
+      expect(panel.hidden).toBe(true);
+      expect(panel.contentClasses()).toEqual({
+        content: true,
+        hidden: true,
+      });
+    });
+
+    it('should display content if not hidden', () => {
+      const panel = new ZenPanel();
+
+      panel.toggleHidden();
+
+      expect(panel.hidden).toBe(false);
+      expect(panel.contentClasses()).toEqual({
+        content: true,
+        hidden: false,
+      });
+    });
+  });
 });
