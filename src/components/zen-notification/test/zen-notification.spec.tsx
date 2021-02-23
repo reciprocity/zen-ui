854ed14c7a0337d4545e5f5197b90db4bc0a360d
Mihael Palfi <mihael@reciprocitylabs.com>
2021-02-23 10:06:25 +0100

    Remove not needed test

--- src/components/zen-notification/test/zen-notification.spec.tsx
+++ src/components/zen-notification/test/zen-notification.spec.tsx
@@ -2,15 +2,6 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ZenNotification } from '../zen-notification';
 
 describe('zen-notification', () => {
-  it.each(['success', 'info', 'warning', 'error'])('should correctly apply variant (variant: %s)', async variant => {
-    const page = await newSpecPage({
-      components: [ZenNotification],
-      html: `<zen-notification variant="${variant}" />`,
-    });
-    const className = '.notification-' + variant;
-    expect(page.root.shadowRoot.querySelector(className)).not.toBeNull();
-  });
-
   it('should set title and message', async () => {
     const page = await newSpecPage({
       components: [ZenNotification],
