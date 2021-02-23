8241cf4956282818bdffc09ada5391db83d91048
Mihael Palfi <mihael@reciprocitylabs.com>
2021-02-23 13:50:17 +0100

    Add close button to component

--- src/components/zen-drawer/zen-drawer.tsx
+++ src/components/zen-drawer/zen-drawer.tsx
@@ -1,6 +1,8 @@
 import { Component, Element, Host, h, Prop, Watch } from '@stencil/core';
 import { hideInstantly, hideWithAnimation, showInstantly, showWithAnimation } from '../helpers/animations';
 import { Position } from '../helpers/types';
+import { applyPrefix } from '../helpers/helpers';
+import { faArrowToRight } from '@fortawesome/pro-light-svg-icons';
 
 @Component({
   tag: 'zen-drawer',
@@ -28,9 +30,20 @@ export class ZenDrawer {
   }
 
   render(): HTMLElement {
+    const ZenButton = applyPrefix('zen-button', this.element);
+    const ZenIcon = applyPrefix('zen-icon', this.element);
     return (
       <Host data-position={this.position}>
         <div class="drawer" data-position={this.position} ref={el => (this.drawer = el)}>
+          <ZenButton
+            class="close-btn"
+            variant="tertiary"
+            onClick={() => {
+              this.element.opened = false;
+            }}
+          >
+            <ZenIcon size="md" class="close-icon" icon={faArrowToRight}></ZenIcon>
+          </ZenButton>
           <slot></slot>
         </div>
       </Host>
