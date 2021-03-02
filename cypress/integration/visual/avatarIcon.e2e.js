97202587fcebe9be251216cf97e502e1a6a13690
Jan Savli <jan.savli@gmail.com>
2021-03-02 14:01:35 +0100

    move avatar components to a sub-group

--- cypress/integration/visual/avatarIcon.e2e.js
+++ cypress/integration/visual/avatarIcon.e2e.js
@@ -4,9 +4,9 @@ import { createVisualTests } from '../../support/utils/visualTesting';
 describe('Avatar icon visual tests', () => {
   const pageId = 'icons-avatar-icon--default';
   const stories = [
-    'story--icons-avatar-icon--story-avatar-variants',
-    'story--icons-avatar-icon--story-avatar-sizes',
-    'story--icons-avatar-icon--default-story',
+    'story--icons-avatar-avatar-icon--story-avatar-variants',
+    'story--icons-avatar-avatar-icon--story-avatar-sizes',
+    'story--icons-avatar-avatar-icon--default-story',
   ];
 
   // Example how to skip testing for some user stories
