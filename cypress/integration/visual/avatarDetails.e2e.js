279658ddfd782ec87454cefbae07f88b2d949884
Mihael Palfi <mihael@reciprocitylabs.com>
2021-03-11 11:36:06 +0100

    Add avatar details vrt

---
+++ cypress/integration/visual/avatarDetails.e2e.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+import { createVisualTests } from '../../support/utils/visualTesting';
+
+describe('Avatar details visual tests', () => {
+  const pageId = 'icons-avatar-avatar-details--default';
+  const stories = ['story--icons-avatar-avatar-details--default-story'];
+
+  // Example how to skip testing for some user stories
+  const skipedStories = [];
+
+  before(() => {
+    cy.visitStorybookIframe(pageId);
+    cy.verifyAllStoriesHaveVRT(stories, skipedStories);
+  });
+
+  createVisualTests(stories, skipedStories, 'sb-zen-avatar-details');
+});
