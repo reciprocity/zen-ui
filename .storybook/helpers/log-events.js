4daf070aa23b7cda008e07dc8b1b0b89914ac219
Jan Savli <jan.savli@gmail.com>
2021-03-17 10:59:32 +0100

    feature: improve logEvents function
    - it now takes component JSON doc data and extracts documented events automatically

--- .storybook/helpers/log-events.js
+++ .storybook/helpers/log-events.js
@@ -5,14 +5,37 @@ const capitalize = (s) => {
   return s.charAt(0).toUpperCase() + s.slice(1);
 };
 
+function docTagToCustomEvent(docTag) {
+  const params = docTag.text.split('|');
+  return {
+    event: (params[0] || '').trim(),
+    bubbles: true,
+    cancelable: true,
+    composed: true,
+    docs: (params[1] || '').trim(),
+    docsTags: [],
+    detail: '',
+  };
+}
+
+const getDocumentedEvents = (componentData) => {
+  // We allow adding extra events to documentation by adding docs-json @event tags.
+  // Here we concat those events with the list of native Stencil @Events.
+  const customEvents = componentData.docsTags
+    ? componentData.docsTags.filter(n => n.name === 'event').map(n => docTagToCustomEvent(n))
+    : [];
+  return componentData.events.concat(customEvents);
+};
+
 export const eventHandles = (eventNames) => {
   return eventNames.map(eventName => `on${capitalize(eventName)}`);
 };
 
-export function logEvents(compName, eventNames) {
+export function logEvents(selector, componentData) {
+  const eventNames = getDocumentedEvents(componentData).map(n => n.event);
   return script(`
     (function(){
-      const comp = document.querySelector('${compName}');
+      const comp = document.querySelector('${selector}');
       ${eventNames.map((eventName) => `
         comp.addEventListener('${eventName}', event => {
           var evt = new CustomEvent('on${capitalize(eventName)}', {
