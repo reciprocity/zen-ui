8dbfd7eca2d38b319e854d36c840a530f7942fe1
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2021-03-22 00:35:21 -0300

    test: added one basic test

--- src/components/zen-skeleton/test/zen-skeleton.spec.tsx
+++ src/components/zen-skeleton/test/zen-skeleton.spec.tsx
@@ -2,17 +2,11 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ZenSkeleton } from '../zen-skeleton';
 
 describe('zen-skeleton', () => {
-  it('renders', async () => {
+  it('should render with shadow dom', async () => {
     const page = await newSpecPage({
       components: [ZenSkeleton],
       html: `<zen-skeleton></zen-skeleton>`,
     });
-    expect(page.root).toEqualHtml(`
-      <zen-skeleton>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </zen-skeleton>
-    `);
+    expect(page.root.shadowRoot).toBeTruthy();
   });
 });
