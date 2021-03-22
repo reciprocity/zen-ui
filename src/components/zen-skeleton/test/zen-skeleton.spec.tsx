df93181b5d450ba3273e0a8d988518c7b2b18c0f
Fermín Jáuregui Lorda <fermin.jaureguil@gmail.com>
2021-03-22 19:38:26 -0300

    test: added more tests to zen-skeleton

--- src/components/zen-skeleton/test/zen-skeleton.spec.tsx
+++ src/components/zen-skeleton/test/zen-skeleton.spec.tsx
@@ -9,4 +9,21 @@ describe('zen-skeleton', () => {
     });
     expect(page.root.shadowRoot).toBeTruthy();
   });
+
+  it('should render a squared skeleton', async () => {
+    const page = await newSpecPage({
+      components: [ZenSkeleton],
+      html: `<zen-skeleton width="100px" height="100px"></zen-skeleton>`,
+    });
+    expect(page.root.style.width).toBe('100px');
+    expect(page.root.style.height).toBe('100px');
+  });
+
+  it('should render a skeleton circle', async () => {
+    const page = await newSpecPage({
+      components: [ZenSkeleton],
+      html: `<zen-skeleton width="100px" height="100px" is-rounded></zen-skeleton>`,
+    });
+    expect(page.root.getAttribute('is-rounded')).toBe('');
+  });
 });
