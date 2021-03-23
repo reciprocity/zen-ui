c36434684deef0c869528f756e8e93a77d2da1f1
Mihael Palfi <mihael@reciprocitylabs.com>
2021-03-23 14:05:59 +0100

    refactor: move sortable to lists menu and update vrt test

--- cypress/integration/visual/sortable.e2e.js
+++ cypress/integration/visual/sortable.e2e.js
@@ -3,12 +3,9 @@
 import { createVisualTests } from '../../support/utils/visualTesting';
 
 describe('Sortable visual tests', () => {
-  const pageId = 'in-progress-sortable-sortable--default';
+  const pageId = 'lists-sortable-sortable--default';
 
-  const stories = [
-    'story--in-progress-sortable-sortable--default-story',
-    'story--in-progress-sortable-sortable--sortable',
-  ];
+  const stories = ['story--lists-sortable-sortable--default-story', 'story--lists-sortable-sortable--sortable'];
 
   const skippedStories = [];
 
