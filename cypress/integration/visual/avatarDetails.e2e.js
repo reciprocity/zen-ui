9844ea1b3f2b8639061a90fee07464332863c1d7
Mihael Palfi <mihael@reciprocitylabs.com>
2021-03-25 12:00:31 +0100

    fix: add and update vrt snapshots

--- cypress/integration/visual/avatarDetails.e2e.js
+++ cypress/integration/visual/avatarDetails.e2e.js
@@ -3,7 +3,10 @@ import { createVisualTests } from '../../support/utils/visualTesting';
 
 describe('Avatar details visual tests', () => {
   const pageId = 'icons-avatar-avatar-details--default';
-  const stories = ['story--icons-avatar-avatar-details--default-story'];
+  const stories = [
+    'story--icons-avatar-avatar-details--default-story',
+    'story--icons-avatar-avatar-details--story-variants',
+  ];
 
   // Example how to skip testing for some user stories
   const skipedStories = [];
