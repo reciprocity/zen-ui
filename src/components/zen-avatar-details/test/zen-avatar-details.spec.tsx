e09802c644604848f2f38227955a975eb9cb821d
Mihael Palfi <mihael@reciprocitylabs.com>
2021-03-27 09:12:47 +0100

    fix: test coverage

--- src/components/zen-avatar-details/test/zen-avatar-details.spec.tsx
+++ src/components/zen-avatar-details/test/zen-avatar-details.spec.tsx
@@ -13,15 +13,51 @@ export const user = [
 ];
 
 describe('zen-avatar-details', () => {
-  it('should render avatar details correctly', async () => {
+  it('should render avatar details variant correctly', async () => {
     const page = await newSpecPage({
       components: [ZenAvatarDetails, ZenAvatarIcon, ZenText],
       html: `<zen-avatar-details />`,
     });
     page.root.users = user;
     expect(page.root.shadowRoot.querySelector('zen-avatar-icon')).toBeTruthy();
+    expect(page.root.shadowRoot.querySelector('[data-test="email"]')).toBeTruthy();
 
     const textElements = page.root.shadowRoot.querySelectorAll('zen-text');
     expect(textElements.length).toEqual(2);
+  });
+
+  it('should render avatar variant basic, without email text', async () => {
+    const page = await newSpecPage({
+      components: [ZenAvatarDetails, ZenAvatarIcon, ZenText],
+      html: `<zen-avatar-details variant="basic" />`,
+    });
+    page.root.users = user;
+    expect(page.root.shadowRoot.querySelector('[data-test="email"]')).not.toBeTruthy();
+  });
+
+  it('should render avatar variant basic, default size', async () => {
+    const page = await newSpecPage({
+      components: [ZenAvatarDetails, ZenAvatarIcon, ZenText],
+      html: `<zen-avatar-details variant="basic" />`,
+    });
+    page.root.users = user;
+    const iconElement = page.root.shadowRoot.querySelector('zen-avatar-icon') as HTMLZenAvatarIconElement;
+    expect(iconElement.size).toEqual('md');
+
+    const textElement = page.root.shadowRoot.querySelector('zen-text') as HTMLZenTextElement;
+    expect(textElement.size).toEqual('lg');
+  });
+
+  it('should render avatar variant basic, large size', async () => {
+    const page = await newSpecPage({
+      components: [ZenAvatarDetails, ZenAvatarIcon, ZenText],
+      html: `<zen-avatar-details variant="basic" size="lg" />`,
+    });
+    page.root.users = user;
+    const iconElement = page.root.shadowRoot.querySelector('zen-avatar-icon') as HTMLZenAvatarIconElement;
+    expect(iconElement.size).toEqual('lg');
+
+    const textElement = page.root.shadowRoot.querySelector('zen-text') as HTMLZenTextElement;
+    expect(textElement.size).toEqual('xl');
   });
 });
