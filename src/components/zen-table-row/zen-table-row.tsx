f788fc2c3133bbaec0f487b78c895f004e7c5139
Mihael Palfi <mihael@reciprocitylabs.com>
2021-03-29 13:26:56 +0200

    fix: remove documented lines for events and methods

--- src/components/zen-table-row/zen-table-row.tsx
+++ src/components/zen-table-row/zen-table-row.tsx
@@ -4,10 +4,6 @@ import { faChevronRight } from '@fortawesome/pro-light-svg-icons';
 
 /**
  * @slot [default] - Content for table cells
- * @event rowSelectChanged | Called on any selection change
- * @method hasRowsSelected | Returns true if descendents have any row selected
- * @method hasAllRowsSelected | Returns true if descendents have all rows selected
- * @method parentRow | Returns parent row
  */
 @Component({
   tag: 'zen-table-row',
