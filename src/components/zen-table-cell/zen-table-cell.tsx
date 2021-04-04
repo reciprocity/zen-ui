17ceb2a3279fac67e10256c7d0c26b84d1876ce0
Jan Savli <jan.savli@gmail.com>
2021-04-04 22:47:40 +0200

    chore: move widgets to cell
    - first cell will have widgets.
      Row can no longer have it since its display: contents).
      Plus widget controls are really posesion of the first cell.

--- src/components/zen-table-cell/zen-table-cell.tsx
+++ src/components/zen-table-cell/zen-table-cell.tsx
@@ -1,4 +1,6 @@
 import { h, Component, Host } from '@stencil/core';
+import { applyPrefix } from '../helpers/helpers';
+import { faChevronRight } from '@fortawesome/pro-light-svg-icons';
 
 @Component({
   tag: 'zen-table-cell',
@@ -7,8 +9,31 @@ import { h, Component, Host } from '@stencil/core';
 })
 export class ZenTableCell {
   render(): HTMLTableCellElement {
+    const ZenCheckBox = applyPrefix('zen-checkbox', this.host);
+    const ZenIcon = applyPrefix('zen-icon', this.host);
     return (
       <Host>
+        {this.showWidgets() && (
+          <div class="widgets">
+            {this.selectable && (
+              <ZenCheckBox
+                indeterminate={this.$indeterminate}
+                class="checkbox"
+                checked={this.selected}
+                onClick={() => this.onSelect()}
+              />
+            )}
+            {this.expandable && (
+              <ZenIcon
+                class="expand-icon"
+                size="sm"
+                padding="lg"
+                icon={faChevronRight}
+                onClick={() => this.onExpand()}
+              />
+            )}
+          </div>
+        )}
         <slot></slot>
       </Host>
     );
--- src/components/zen-table-row/zen-table-row.tsx
+++ src/components/zen-table-row/zen-table-row.tsx
@@ -1,6 +1,4 @@
 import { h, Component, Host, Prop, Element, Event, EventEmitter, Method, Watch } from '@stencil/core';
-import { applyPrefix } from '../helpers/helpers';
-import { faChevronRight } from '@fortawesome/pro-light-svg-icons';
 
 /**
  * @slot [default] - Content for table cells
@@ -162,31 +160,8 @@ export class ZenTableRow {
   }
 
   render(): HTMLTableRowElement {
-    const ZenCheckBox = applyPrefix('zen-checkbox', this.host);
-    const ZenIcon = applyPrefix('zen-icon', this.host);
     return (
       <Host>
-        {this.showWidgets() && (
-          <div class="widgets">
-            {this.selectable && (
-              <ZenCheckBox
-                indeterminate={this.$indeterminate}
-                class="checkbox"
-                checked={this.selected}
-                onClick={() => this.onSelect()}
-              />
-            )}
-            {this.expandable && (
-              <ZenIcon
-                class="expand-icon"
-                size="sm"
-                padding="lg"
-                icon={faChevronRight}
-                onClick={() => this.onExpand()}
-              />
-            )}
-          </div>
-        )}
         <slot></slot>
       </Host>
     );
