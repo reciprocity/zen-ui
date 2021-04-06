bc041277a7eb56a0f6df7a6f8de03da5ba38ecce
homer0 <leo.apiwan@reciprocitylabs.com>
2021-04-06 14:58:00 -0300

    chore: setup react bindings build

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,9 +1,15 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'zen-ui',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '..',
+      proxiesFile: './react/components.ts',
+      includeDefineCustomElements: false,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
