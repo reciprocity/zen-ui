0479b38eb4f9cf6517f6cebd25be36fde763c1f0
Jan Savli <jan.savli@gmail.com>
2021-04-08 21:28:08 +0200

    chore: add child mutation observer to table
    - we'll use it to cleanup table structure on any child change

--- src/components/zen-table/zen-table.tsx
+++ src/components/zen-table/zen-table.tsx
@@ -6,15 +6,38 @@ import { h, Component, Host, Element, Prop } from '@stencil/core';
   shadow: true,
 })
 export class ZenTable {
+  childObserver: MutationObserver = null;
+
   @Element() host: HTMLZenTableElement;
 
   /** Space separated css grid columns<br/>(eg. `auto 1fr 1fr 200px 1fr`) */
   @Prop() readonly columns = '';
 
+  /** Private variable (table cleanup in progress) */
+  async cleanupTableStructure(): Promise<void> {
+    // todo
+  }
+
+  startChildObserver(): void {
+    this.childObserver = new MutationObserver(() => this.cleanupTableStructure());
+
+    this.childObserver.observe(this.host, {
+      childList: true,
+      attributes: true,
+      subtree: true,
+    });
+  }
+
   async componentDidLoad(): Promise<void> {
     if (!this.columns) {
       console.error('Zen-table error: Missing prop `columns`!', this.host);
     }
+
+    this.startChildObserver();
+  }
+
+  disconnectedCallback(): void {
+    this.childObserver.disconnect();
   }
 
   render(): HTMLTableElement {
