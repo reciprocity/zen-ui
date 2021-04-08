67bfd88a1f5d181353c1cb6d2451c9e7faae5a56
Mihael Palfi <mihael@reciprocitylabs.com>
2021-04-08 07:21:02 +0200

    test(zen-tab): add unit tests

--- src/components/zen-tab/test/zen-tab.spec.tsx
+++ src/components/zen-tab/test/zen-tab.spec.tsx
@@ -2,12 +2,23 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ZenTab } from '../zen-tab';
 
 describe('zen-tab', () => {
-  it('renders', async () => {
+  it('should render', async () => {
     const page = await newSpecPage({
       components: [ZenTab],
       html: `<zen-tab></zen-tab>`,
     });
 
     expect(page.root.shadowRoot).toBeTruthy();
+  });
+
+  it('should select', async () => {
+    const page = await newSpecPage({
+      components: [ZenTab],
+      html: `<zen-tab></zen-tab>`,
+    });
+    page.rootInstance.selected = true;
+
+    await page.waitForChanges();
+    expect(page.root.classList.contains('selected')).toBeTruthy();
   });
 });
