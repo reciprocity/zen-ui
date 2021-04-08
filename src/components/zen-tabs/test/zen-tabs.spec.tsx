865dde8c8ce150ede0a6b4498bcdc586be355572
Mihael Palfi <mihael@reciprocitylabs.com>
2021-04-08 07:23:11 +0200

    test(zen-tabs): add zen tabs unit tests

--- src/components/zen-tabs/test/zen-tabs.spec.tsx
+++ src/components/zen-tabs/test/zen-tabs.spec.tsx
@@ -1,40 +1,40 @@
 import { newSpecPage, SpecPage } from '@stencil/core/testing';
 
 import { ZenTabs } from '../zen-tabs';
+import { ZenTab } from '../../zen-tab/zen-tab';
+import { simulateMouse } from '../../helpers/jest';
 
 describe('zen-tabs', () => {
-  describe('tabs with items', () => {
-    let page: SpecPage;
-    let tabs: NodeListOf<Element>;
+  let page: SpecPage;
+  let tabs: HTMLZenTabElement[];
 
-    beforeEach(async () => {
-      page = await newSpecPage({
-        components: [ZenTabs],
-        html: `<zen-tabs />`,
-      });
-      page.rootInstance.tabs = [
-        { label: 'Tab 1', value: '1' },
-        { label: 'Tab 2', value: '2' },
-      ];
-      await page.waitForChanges();
-      tabs = page.root.shadowRoot.querySelectorAll('.tab');
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [ZenTabs, ZenTab],
+      html: `<zen-tabs value="0">
+                <zen-tab>tab1</zen-tab>
+                <zen-tab>tab2</zen-tab>
+             </zen-tabs>`,
     });
+    await page.waitForChanges();
+    tabs = Array.from(page.root.children).map(n => n as HTMLZenTabElement);
+  });
 
-    it('should render tabs', async () => {
-      expect(tabs.length).toEqual(2);
-    });
+  it('should render tabs', async () => {
+    expect(page.root.shadowRoot).toBeTruthy();
+    expect(tabs.length).toEqual(2);
+  });
 
-    it('should render selected tab', async () => {
-      page.rootInstance.value = '2';
-      await page.waitForChanges();
-      expect(tabs[1].classList.contains('active')).toBeTruthy();
-    });
+  it('should render selected tab', async () => {
+    page.rootInstance.value = '1';
+    await page.waitForChanges();
+    expect(tabs[1].classList.contains('selected')).toBeTruthy();
+  });
 
-    it('should select tab on click', async () => {
-      expect(tabs[0].classList.contains('active')).toBeFalsy();
-      tabs[0].click();
-      await page.waitForChanges();
-      expect(tabs[0].classList.contains('active')).toBeTruthy();
-    });
+  it('should select tab on click', async () => {
+    expect(tabs[1].classList.contains('selected')).toBeFalsy();
+    await simulateMouse('click', tabs[1]);
+    await page.waitForChanges();
+    expect(tabs[0].classList.contains('selected')).toBeTruthy();
   });
 });
