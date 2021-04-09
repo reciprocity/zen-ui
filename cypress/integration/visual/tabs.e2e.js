bb512c5b9ee71cfede7598dbc6368e2cd4101b26
Mihael Palfi <mihael@reciprocitylabs.com>
2021-04-09 09:17:03 +0200

    feat(zen-tabs): move tabs to story navigation folder

--- cypress/integration/visual/tabs.e2e.js
+++ cypress/integration/visual/tabs.e2e.js
@@ -2,8 +2,8 @@
 import { createVisualTests } from '../../support/utils/visualTesting';
 
 describe('Tabs visual tests', () => {
-  const pageId = 'in-progress-tabs-tabs--default';
-  const stories = ['story--in-progress-tabs-tabs--default-story'];
+  const pageId = 'navigation-tabs-tabs--default';
+  const stories = ['story--navigation-tabs-tabs--default-story'];
 
   before(() => {
     cy.visitStorybookIframe(pageId);
