373462be3e7165bb84cb920bbab67c9620513fa0
Jan Savli <jan.savli@gmail.com>
2021-04-13 14:32:40 +0200

    chore: move fonts to storybook folder

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = async ({ config }) => {
   config.plugins.push(
     new CopyWebpackPlugin({
       patterns: [{
-        from: path.resolve(__dirname, '../src/fonts'),
+        from: path.resolve(__dirname, './fonts'),
         to: 'static/fonts'
       }]
     }),
