323bb211d823ae02383a7ef66d8749c5aba443d7
Mihael Palfi <mihael@reciprocitylabs.com>
2021-04-13 09:40:21 +0200

    test(zen-card): update snapshots

--- cypress/integration/visual/card.e2e.js
+++ cypress/integration/visual/card.e2e.js
@@ -5,7 +5,11 @@ import { createVisualTests } from '../../support/utils/visualTesting';
 describe('Card visual tests', () => {
   const pageId = 'containers-card--default';
 
-  const stories = ['story--containers-card--disabled', 'story--containers-card--default-story'];
+  const stories = [
+    'story--containers-card--variant',
+    'story--containers-card--disabled',
+    'story--containers-card--default-story',
+  ];
 
   before(() => {
     cy.visitStorybookIframe(pageId);
