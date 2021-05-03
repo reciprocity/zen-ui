bd22d2ffd26a5b585baefa29e4b13e71dc59ae32
Mihael Palfi <mihael@reciprocitylabs.com>
2021-05-03 10:06:44 +0200

    feat(zen-tab): add disabled and badge params

--- src/components/zen-tab/zen-tab.tsx
+++ src/components/zen-tab/zen-tab.tsx
@@ -8,20 +8,27 @@ import { Component, Host, h, Event, EventEmitter, Element, Prop } from '@stencil
 export class ZenTab {
   @Element() host: HTMLZenTabElement;
 
-  /** Set tab selected */
+  /** Tab selected */
   @Prop({ reflect: true }) readonly selected: boolean = false;
+
+  /** Tab disabled */
+  @Prop({ reflect: true }) readonly disabled: boolean = false;
+
+  /** Tab badge value (null to not display it) */
+  @Prop() readonly badge: string | null = null;
 
   /** Tab selected event */
   @Event() zenSelect: EventEmitter<void>;
 
   onClick(): void {
-    this.zenSelect.emit();
+    if (!this.disabled) this.zenSelect.emit();
   }
 
   render(): HTMLZenTabElement {
     return (
       <Host onClick={() => this.onClick()}>
         <slot></slot>
+        {this.badge !== null && <div class="badge">{this.badge}</div>}
       </Host>
     );
   }
