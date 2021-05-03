9e5fe573104daab8acff23d1ccdfd40c9141b28c
Mihael Palfi <mihael@reciprocitylabs.com>
2021-05-03 12:28:04 +0200

    test(zen-tab): update unit test cases

--- src/components/zen-tab/test/zen-tab.spec.tsx
+++ src/components/zen-tab/test/zen-tab.spec.tsx
@@ -1,5 +1,6 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { ZenTab } from '../zen-tab';
+import { simulateMouse } from '../../helpers/jest';
 
 describe('zen-tab', () => {
   it('should render', async () => {
@@ -9,5 +10,37 @@ describe('zen-tab', () => {
     });
 
     expect(page.root.shadowRoot).toBeTruthy();
+  });
+
+  it('should display a badge', async () => {
+    const page = await newSpecPage({
+      components: [ZenTab],
+      html: `<zen-tab badge="123">Tab 1</zen-tab>`,
+    });
+
+    expect(page.root.shadowRoot.querySelector('.badge')).toBeTruthy();
+  });
+
+  it('should be disabled', async () => {
+    const page = await newSpecPage({
+      components: [ZenTab],
+      html: `<zen-tab disabled>Tab 1</zen-tab>`,
+    });
+
+    expect(page.root.hasAttribute('disabled')).toBeTruthy();
+  });
+
+  it('should trigger on click event', async () => {
+    const page = await newSpecPage({
+      components: [ZenTab],
+      html: `<zen-tab>Tab 1</zen-tab>`,
+    });
+
+    const clickSpy = jest.fn();
+    page.root.addEventListener('click', clickSpy);
+    simulateMouse('click', page.root);
+    await page.waitForChanges();
+
+    expect(clickSpy).toHaveBeenCalled();
   });
 });
