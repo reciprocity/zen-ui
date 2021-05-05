17af03bc91825a0e867daf669ac6489cbe2df192
Jan Savli <jan.savli@gmail.com>
2021-05-05 13:41:01 +0200

    chore: add skeletons

--- src/components/zen-sidebar-nav-skeleton/zen-sidebar-nav-skeleton.tsx
+++ src/components/zen-sidebar-nav-skeleton/zen-sidebar-nav-skeleton.tsx
@@ -1,4 +1,5 @@
-import { Component, Host, h, Prop } from '@stencil/core';
+import { Component, Host, h, Prop, Element } from '@stencil/core';
+import { applyPrefix } from '../helpers/helpers';
 
 @Component({
   tag: 'zen-sidebar-nav-skeleton',
@@ -6,13 +7,25 @@ import { Component, Host, h, Prop } from '@stencil/core';
   shadow: true,
 })
 export class ZenSidebarNavSkeleton {
+  @Element() host: HTMLZenSidebarNavSkeletonElement;
+
   /** Width of sidebar in maximized state (css prop)  */
   @Prop() readonly width: string = '13.5rem';
 
+  private widths = [...Array(6)].map(() => 50 + Math.random() * 0.5 * 100);
+
   render(): HTMLElement {
+    const ZenSpace = applyPrefix('zen-space', this.host);
+    const ZenSkeleton = applyPrefix('zen-skeleton', this.host);
     return (
       <Host style={{ width: this.width }}>
-        <p>todo</p>
+        <ZenSpace block padding="none">
+          <ZenSpace padding="md" spacing="xl" vertical>
+            {this.widths.map(width => (
+              <ZenSkeleton style={{ width: `${width}%`, height: '1rem' }}></ZenSkeleton>
+            ))}
+          </ZenSpace>
+        </ZenSpace>
       </Host>
     );
   }
