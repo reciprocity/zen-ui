bad144c2d9b3a6b1d501b16e7985e30f49f50dd1
Jan Savli <jan.savli@gmail.com>
2021-05-07 16:36:20 +0200

    docs: add story for program-selector

--- cypress/integration/visual/sidebarNavSkeleton.e2e.js
+++ cypress/integration/visual/sidebarNavSkeleton.e2e.js
@@ -3,7 +3,10 @@ import { createVisualTests } from '../../support/utils/visualTesting';
 
 describe('Sidebar Nav Skeleton visual tests', () => {
   const pageId = 'navigation-sidebar-navigation-sidebar-nav-skeleton';
-  const stories = ['story--navigation-sidebar-navigation-sidebar-nav-skeleton--default-story'];
+  const stories = [
+    'story--navigation-sidebar-navigation-sidebar-nav-skeleton--default-story',
+    'story--navigation-sidebar-navigation-sidebar-nav-skeleton--programs-selector',
+  ];
 
   // Example how to skip testing for some user stories
   const skipedStories = [];
