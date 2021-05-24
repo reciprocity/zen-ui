8c5addefec3b3630d10d265decfe4dd132397099
Mihael Palfi <mihael@reciprocitylabs.com>
2021-05-24 14:35:29 +0200

    test(zen-badge): add unit tests

--- src/components/zen-badge/test/zen-badge.spec.tsx
+++ src/components/zen-badge/test/zen-badge.spec.tsx
@@ -2,17 +2,25 @@ import { newSpecPage } from '@stencil/core/testing';
 import { ZenBadge } from '../zen-badge';
 
 describe('zen-badge', () => {
-  it('renders', async () => {
+  it('should render badge', async () => {
     const page = await newSpecPage({
       components: [ZenBadge],
-      html: `<zen-badge></zen-badge>`,
+      html: `<zen-badge>Content</zen-badge>`,
     });
-    expect(page.root).toEqualHtml(`
-      <zen-badge>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </zen-badge>
-    `);
+
+    expect(page.root.innerHTML).toEqual('Content');
+  });
+
+  it('should be invisible', async () => {
+    const page = await newSpecPage({
+      components: [ZenBadge],
+      html: `<zen-badge value="123"></zen-badge>`,
+    });
+
+    const badge = page.root as HTMLZenBadgeElement;
+    badge.value = null;
+    await page.waitForChanges();
+
+    expect(page.root.classList.contains('hidden')).toBe(true);
   });
 });
